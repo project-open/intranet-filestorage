54067991b87a91bbd0246eb7229c48a70be03ca0
Frank Bergmann <frank.bergmann@project-open.com>
2006-01-20 17:58:08 +0000

    - added privileges for default FS root

--- sql/postgresql/intranet-filestorage-create.sql
+++ sql/postgresql/intranet-filestorage-create.sql
@@ -287,3 +287,56 @@ SELECT im_component_plugin__new (
 
 
 
+
+-- -----------------------------------------------------
+-- Add privileges to handle the default privileges on
+-- empty filestorages
+
+select acs_privilege__create_privilege('fs_root_view','Default view privilege for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_view');
+
+select acs_privilege__create_privilege('fs_root_read','Default read privilege for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_read');
+
+select acs_privilege__create_privilege('fs_root_write','Default write privilege for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_write');
+
+select acs_privilege__create_privilege('fs_root_admin','Default admin privilege for FS root?','');
+select acs_privilege__add_child('admin', 'fs_root_admin');
+
+
+-- View Privileges
+--
+select im_priv_create('fs_root_view',        'Employees');
+select im_priv_create('fs_root_view',        'Accounting');
+select im_priv_create('fs_root_view',        'P/O Admins');
+select im_priv_create('fs_root_view',        'Project Managers');
+select im_priv_create('fs_root_view',        'Senior Managers');
+
+
+-- Read Privileges
+--
+select im_priv_create('fs_root_read',        'Employees');
+select im_priv_create('fs_root_read',        'Accounting');
+select im_priv_create('fs_root_read',        'P/O Admins');
+select im_priv_create('fs_root_read',        'Project Managers');
+select im_priv_create('fs_root_read',        'Senior Managers');
+
+
+-- Write Privileges
+--
+select im_priv_create('fs_root_write',        'Employees');
+select im_priv_create('fs_root_write',        'Accounting');
+select im_priv_create('fs_root_write',        'P/O Admins');
+select im_priv_create('fs_root_write',        'Project Managers');
+select im_priv_create('fs_root_write',        'Senior Managers');
+
+
+-- Admin Privileges
+--
+select im_priv_create('fs_root_admin',        'Employees');
+select im_priv_create('fs_root_admin',        'Accounting');
+select im_priv_create('fs_root_admin',        'P/O Admins');
+select im_priv_create('fs_root_admin',        'Project Managers');
+select im_priv_create('fs_root_admin',        'Senior Managers');
+
