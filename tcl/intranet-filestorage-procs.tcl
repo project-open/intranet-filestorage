777a013e9c3cebeedddd67b652289ce1c5712e36
Frank Bergmann <frank.bergmann@project-open.com>
2019-07-02 14:36:53 +0000

    - changed name for HTML form $folder_type to fs_${folder_type}   in order to avoid conflict with ad_form in new page

--- tcl/intranet-filestorage-procs.tcl
+++ tcl/intranet-filestorage-procs.tcl
@@ -1189,15 +1189,21 @@ ad_proc im_filestorage_tool_tds { folder folder_type project_id return_url up_li
   modifying the URL variables. Doesn't work yet 100%, so better
   disable meanwhile...
    <td>
-     <input type=image src=/intranet/images/upfolder.gif width=21 height=21 onClick=\"window.document.$folder_type.actions.value='up-folder'; submit();\" title='[_ intranet-filestorage.Folder_up]' alt='[_ intranet-filestorage.Folder_up]'>
-   </td>--><td><input type=image src=/intranet/images/folder-add.png width=16 height=16 onClick=\"window.document.$folder_type.actions.value='new-folder'; submit();\" title='[_ intranet-filestorage.Create_a_new_folder]' alt='[_ intranet-filestorage.Create_a_new_folder]'>
-   </td><td><input type=image src=/intranet/images/add.png width=16 height=16 onClick=\"window.document.$folder_type.actions.value='upload'; submit();\" title='[_ intranet-filestorage.Upload_a_file]' alt='[_ intranet-filestorage.Upload_a_file]'>
-   </td><!--<td><input type=image src=/intranet/images/new-doc.gif width=21 height=21 onClick=\"window.document.$folder_type.actions.value='new-doc'; submit();\" title='[_ intranet-filestorage.lt_Create_a_new_document]' alt='[_ intranet-filestorage.lt_Create_a_new_document]'>
-   </td>--><td><input type=image src=/intranet/images/cancel.png width=16 height=16 onClick=\"window.document.$folder_type.actions.value='del'; submit();\" title='[_ intranet-filestorage.lt_Delete_files_and_fold]' alt='[_ intranet-filestorage.lt_Delete_files_and_fold]'>
-   </td><td><input type=image src=/intranet/images/zip-download.gif width=16 height=16 onClick=\"window.document.$folder_type.actions.value='zip'; submit();\" title='[_ intranet-filestorage.lt_Download_all_files_as]' alt='[_ intranet-filestorage.lt_Download_all_files_as]'>
-   </td><td><input type=image src=/intranet/images/zip-upload.gif width=16 height=16 onClick=\"window.document.$folder_type.actions.value='unzip'; submit();\" title='[lang::message::lookup "" intranet-filestorage.Upload_ZIP "Upload ZIP"]' alt='[lang::message::lookup "" intranet-filestorage.Upload_ZIP "Upload ZIP"]'>
-   </td><td><input type=image src=/intranet/images/lock-add.png width=16 height=16 onClick=\"window.document.$folder_type.actions.value='add-perms'; submit();\" title='[_ intranet-filestorage.lt_Add_permissions_to_fo]' alt='[_ intranet-filestorage.lt_Add_permissions_to_fo]'>
-    </td><td><input type=image src=/intranet/images/lock-delete.png width=16 height=16 onClick=\"window.document.$folder_type.actions.value='del-perms'; submit();\" title='[_ intranet-filestorage.lt_Remove_permissions_fr]' alt='[_ intranet-filestorage.lt_Remove_permissions_fr]'>
+     <input type=image src=/intranet/images/upfolder.gif width=21 height=21 onClick=\"window.document.fs_${folder_type}.actions.value='up-folder'; submit();\" title='[_ intranet-filestorage.Folder_up]' alt='[_ intranet-filestorage.Folder_up]'>
+   </td> 
+-->
+    <td><input type=image src=/intranet/images/folder-add.png width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='new-folder'; submit();\" title='[_ intranet-filestorage.Create_a_new_folder]' alt='[_ intranet-filestorage.Create_a_new_folder]'>
+   </td><td><input type=image src=/intranet/images/add.png width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='upload'; submit();\" title='[_ intranet-filestorage.Upload_a_file]' alt='[_ intranet-filestorage.Upload_a_file]'>
+   </td>
+<!--
+   <td><input type=image src=/intranet/images/new-doc.gif width=21 height=21 onClick=\"window.document.fs_${folder_type}.actions.value='new-doc'; submit();\" title='[_ intranet-filestorage.lt_Create_a_new_document]' alt='[_ intranet-filestorage.lt_Create_a_new_document]'>
+   </td> 
+-->
+   <td><input type=image src=/intranet/images/cancel.png width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='del'; submit();\" title='[_ intranet-filestorage.lt_Delete_files_and_fold]' alt='[_ intranet-filestorage.lt_Delete_files_and_fold]'>
+   </td><td><input type=image src=/intranet/images/zip-download.gif width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='zip'; submit();\" title='[_ intranet-filestorage.lt_Download_all_files_as]' alt='[_ intranet-filestorage.lt_Download_all_files_as]'>
+   </td><td><input type=image src=/intranet/images/zip-upload.gif width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='unzip'; submit();\" title='[lang::message::lookup "" intranet-filestorage.Upload_ZIP "Upload ZIP"]' alt='[lang::message::lookup "" intranet-filestorage.Upload_ZIP "Upload ZIP"]'>
+   </td><td><input type=image src=/intranet/images/lock-add.png width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='add-perms'; submit();\" title='[_ intranet-filestorage.lt_Add_permissions_to_fo]' alt='[_ intranet-filestorage.lt_Add_permissions_to_fo]'>
+    </td><td><input type=image src=/intranet/images/lock-delete.png width=16 height=16 onClick=\"window.document.fs_${folder_type}.actions.value='del-perms'; submit();\" title='[_ intranet-filestorage.lt_Remove_permissions_fr]' alt='[_ intranet-filestorage.lt_Remove_permissions_fr]'>
     </td>
   </td>
 "
@@ -1939,7 +1945,7 @@ ad_proc -public im_filestorage_base_component { user_id object_id object_name ba
     set profile_tds [im_filestorage_profile_tds $user_id $object_id]
 
     set component_html "
-<form name=\"$folder_type\" method=POST action=\"/intranet-filestorage/action\">
+<form name=\"fs_${folder_type}\" method=POST action=\"/intranet-filestorage/action\">
 [export_vars -form {object_id bread_crum_path folder_type return_url}]
 
 <TABLE border=0 cellpadding=0 cellspacing=0>
